2eb5ae11dc89d129fe65117465c7e239aaa40043
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-11-09 20:29:20 -0300

    fix: ajustando lint de importação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { fetchToken } from '../redux/actions';
 import { Link } from 'react-router-dom';
+import { fetchToken } from '../redux/actions';
 
 class Login extends React.Component {
   constructor() {
