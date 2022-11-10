37a1fbc7daa5fbbada8081ff5416609109233e28
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-11-10 17:21:19 -0300

    Feat: Criado componente Header

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,14 +1,25 @@
-// import { FAZER_LOGIN } from '../actions';
+import { FAZER_LOGIN, RECEIVE_GRAVATAR } from '../actions';
 
 const INITIAL_STATE = {
   name: '',
   assertions: '',
-  score: '',
+  score: 0,
   gravatarEmail: '',
 };
 
 const player = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case FAZER_LOGIN:
+    return ({
+      ...state,
+      name: action.name,
+      gravatarEmail: action.gravatarEmail,
+    });
+  case RECEIVE_GRAVATAR:
+    return ({
+      ...state,
+      gravatarEmail: action.gravatarEmail,
+    });
   default:
     return state;
   }
