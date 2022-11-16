2dac9f59411dda46816f79c4d826fb01f190b62a
Paulo <paulinho-psp@hotmail.com>
2022-11-16 19:50:54 -0300

    Requisito 17 Finalizado

---
+++ src/tests/Feedback.test.js
@@ -0,0 +1,50 @@
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import App from "../App";
+
+describe("Testes da página de Feedback", () => {
+    const nameId = "input-player-name";
+    const emailId = "input-gravatar-email";
+    const buttonId = "btn-play";
+
+    it("Verificando os elementos da página de Feedback", async () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        history.push('/feedback');
+        await waitFor(() => {
+            expect(screen.getByTestId("header-profile-picture")).toBeInTheDocument();
+        });
+        expect(screen.getByTestId("header-score")).toBeInTheDocument();
+        expect(screen.getByTestId("feedback-text")).toBeInTheDocument();
+        expect(screen.getByTestId("feedback-total-score")).toBeInTheDocument();
+        expect(screen.getByTestId("feedback-total-question")).toBeInTheDocument();
+        expect(screen.getByTestId("btn-play-again")).toHaveTextContent('Play Again');
+        expect(screen.getByTestId("btn-ranking")).toHaveTextContent('Ranking');
+    });
+
+    it("Verificando o botão 'Ranking' de Feedback", async () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        history.push('/feedback');
+        await waitFor(() => {
+            expect(screen.getByTestId("header-profile-picture")).toBeInTheDocument();
+        });
+        userEvent.click(screen.getByTestId("btn-ranking"));
+        await waitFor(() => {
+            expect(screen.getByTestId('ranking-title')).toBeInTheDocument();
+        });
+        expect(history.location.pathname).toBe("/ranking");
+    });
+
+    it("Verificando o botão 'Play Again' de Feedback", async () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        history.push('/feedback');
+        await waitFor(() => {
+            expect(screen.getByTestId("header-profile-picture")).toBeInTheDocument();
+        });
+        userEvent.click(screen.getByTestId("btn-play-again"));
+        await waitFor(() => {
+            expect(screen.getByTestId(testIdName)).toBeInTheDocument();
+        });
+        expect(history.location.pathname).toBe("/");
+    });
+});
